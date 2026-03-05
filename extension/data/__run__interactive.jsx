7beb4a8a8a3d1f82c391e0f28c6d1bbe59236ffd
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-03-05 08:30:20 +0100

    Update and rename tests/__run__interactive.js to extension/data/__run__interactive.jsx

--- extension/data/__run__interactive.jsx
+++ extension/data/__run__interactive.jsx
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const { execSync } = require('child_process');
-
 const modules = [
   { name: 'Cart / Utils', file: 'tests/unit/test_cart.js' },
   { name: 'Checkout / Integration', file: 'tests/integration/test_cart_checkout.js' },
